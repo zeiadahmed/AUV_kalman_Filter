778fcd53331d3e4a5e2156b639db96308fd74d14
zeiad <zeiadahmed60@gmail.com>
2022-12-20 01:12:54 +0200

    resolving velocities to world frame

--- build/kalmenfilter_cpp_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zeiad/sensorfusion_ros2_ws/src/kalmenfilter_cpp_pkg
-# Build directory: /home/zeiad/sensorfusion_ros2_ws/build/kalmenfilter_cpp_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
